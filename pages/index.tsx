64dd2c10c154a685964a5217842f2bc0d3fb8a69
Tobias Rosa <tobias.rosa2000@gmail.com>
2022-06-12 23:07:01 -0300

    Added colors and fonts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
 
 const Home: NextPage = () => {
 	return (
@@ -12,7 +11,10 @@ const Home: NextPage = () => {
 					content='A portfolio as a frontend developer focused on performance, SEO and UX.'
 				/>
 			</Head>
-			<div className='container m-auto'>Teste</div>
+			<div className='container mx-auto'>
+				<span>Tobias Rosa</span>
+				<a href='#'>teste</a>
+			</div>
 		</>
 	);
 };
